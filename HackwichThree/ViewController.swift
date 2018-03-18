c7c2bee199b13322717f7caa5107e8d68495bdc2
Jaleeljs <35237438+Jaleeljs@users.noreply.github.com>
2018-03-17 21:58:10 -1000

    finished part one of project 2

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -11,15 +11,35 @@ import UIKit
 class ViewController: UIViewController {
   
     @IBOutlet weak var firstlabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var forthLabel: UILabel!
+    @IBOutlet weak var fifthLabel: UILabel!
     
     
-
+    @IBOutlet weak var aboutMeTextView: UITextView!
+    
+    
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var logoPic: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //set firstlabel to string "About"
 
-self.firstlabel.text =  "About"
+        self.firstlabel.text =  "My Name Is:"
+        self.secondLabel.text = "Jaleel S."
+        self.thirdLabel.text = "My Major Is:"
+        self.forthLabel.text = "Video Game Design"
+        self.fifthLabel.text = "About Me:"
+        
+        self.aboutMeTextView.text = "I'm 21 years old, I like to play video games, I work at Dunkin' Donuts, I'm a full time student at UHWO"
+        
+        profilePic.image = UIImage(named:"IMG_0358")
+        logoPic.image = UIImage(named:"dunkin-donuts-iron-on-wall-stickers-3")
+        
         
         
         
