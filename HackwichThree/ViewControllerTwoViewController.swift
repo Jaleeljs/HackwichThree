88bcc635052970ed1bfc350d42a821412d554089
Jaleeljs <35237438+Jaleeljs@users.noreply.github.com>
2018-03-18 14:37:18 -1000

    finished weather app

--- HackwichThree/ViewControllerTwoViewController.swift
+++ HackwichThree/ViewControllerTwoViewController.swift
@@ -8,22 +8,22 @@
 
 import UIKit
 
-class ViewControllerTwoViewController: UIViewController {
-
+class VewiControllerTwo: UIViewController {
     
-    @IBOutlet var firstTemp: UITextView!
-    @IBOutlet weak var convertedTemp: UILabel!
+
+    @IBOutlet var firstTemp: UITextField!
+    
+   
+    @IBOutlet var convertedTemp: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-       
-
+        
+        firstTemp.placeholder = "Degrees farenheit"
         
     }
-
-    
     
     @IBAction func convertButtonPressed(_ sender: Any) {
         var initialInput: Float
@@ -32,11 +32,15 @@ class ViewControllerTwoViewController: UIViewController {
         t = String((initialInput - 32) * 5/9)
         self.convertedTemp.text = "Today is \(t) degrees celcius."
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
     }
-    
+        
+        
+        
+    }
 
-}
